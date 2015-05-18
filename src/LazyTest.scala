edfb01000355e5288ee0f7c110af55afbcb8f755
Jay Liang <roackb2@gmail.com>
2015-05-18 21:52:26 +0800

    add getTitle of predefined json file to LazyTest

---
+++ src/LazyTest.scala
@@ -0,0 +1,27 @@
+/**
+ * Created by roackb2 on 15/5/18.
+ */
+object LazyTest extends App {
+
+    def getRepositories(account: String): List[String] = {
+        lazy val source = {
+            println("getting repositories...")
+            scala.io.Source.fromURL("https://github.com/" + account).getLines().toList
+        }
+
+        source.filter(_.contains("class=\"repo\"")).map((line) => line.substring(line.indexOf("title=\"") + 7, line.indexOf("\">")))
+    }
+
+    def getTitles(path: String): List[String] = {
+        lazy val file = {
+            println("reading file...")
+            scala.io.Source.fromFile(path).getLines().toList
+        }
+
+        file.filter(_.contains("title")).map((line) => line.substring(line.indexOf("\"title\": \"") + 10, line.indexOf("\",")).toLowerCase())
+    }
+
+//    getRepositories("roackb2")
+
+    getTitles("/Users/roackb2/Studio/programming/scala/test/src/reuters.json").foreach((title) => println(title))
+}
